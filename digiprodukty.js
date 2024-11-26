50e2871a694b7399579621271eb24c68b49a2ee0
menoprjiezvisko <meno.prjiezvisko@gmail.com>
2024-11-26 16:18:20 +0100

    Add files via upload

---
+++ digiprodukty.js
@@ -0,0 +1,33 @@
+    const products = {
+      1: {
+        name: "HNonline",
+        slug: "hnonline",
+        description: "Jediný ekonomický denník na Slovensku, ktorý svojím zameraním prináša seriózne a dôveryhodné informácie pre každého, koho zaujíma ekonomický pohľad na dianie doma a vo svete. Každý pracovný deň prinášajú aktuálne spravodajstvo, analýzy, štatistiky, prognózy a užitočné rady aj pre Váš druh podnikania.",
+        kontakt: "Kontakt: inzercia@mafraslovakia.sk",
+        image: "images/port_hnonline.png",
+        btnCennik: "Cenník",
+        btnCennikUrl: "", 
+        btnPred:"Predplatné",
+        btnPredUrl: "", 
+      },
+    2: {
+        name: "Svetevity",
+        description: "svetevity",
+        kontakt: "Kontakt: inzercia@mafraslovakia.sk",
+        image: "images/port_svetevity.png",
+        btnCennik: "Cenník",
+        btnCennikUrl: "",  
+        btnPred:"Predplatné",
+        btnPredUrl: "", 
+      },
+    3: {
+        name: "Brainee",
+        description: "brainee",
+        kontakt: "Kontakt: inzercia@mafraslovakia.sk",
+        image: "images/port_brainee.png",
+        btnCennik: "Cenník",
+        btnCennikUrl: "",  
+        btnPred:"Predplatné",
+        btnPredUrl: "", 
+      },
+    };
